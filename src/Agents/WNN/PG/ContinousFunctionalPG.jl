9a06913fe276d914b6857b3cbb3bc782a0635bdc
Rafael F. Katopodis <rafaelkatopodis@gmail.com>
2021-12-30 11:38:30 -0300

    Deterministic action selection for continuous policy distributions

--- src/Agents/WNN/PG/ContinousFunctionalPG.jl
+++ src/Agents/WNN/PG/ContinousFunctionalPG.jl
@@ -1,9 +1,9 @@
 using Ramnet.Encoders
 
-using ..Approximators:ContinousActionPolicy
+using ..Approximators: ContinousActionPolicy
 using ..Buffers: MultiStepDynamicBuffer, add!
 
-mutable struct ContinousFunctionalPG{OS,AS,T <: Real,O <: StaticArray{Tuple{OS},T,1},A <: StaticArray{Tuple{AS},T,1},E <: AbstractEncoder{T}} <: AbstractAgent{O,A}
+mutable struct ContinousFunctionalPG{OS,AS,T<:Real,O<:StaticArray{Tuple{OS},T,1},A<:StaticArray{Tuple{AS},T,1},E<:AbstractEncoder{T}} <: AbstractAgent{O,A}
     γ::Float64
     policy::ContinousActionPolicy{OS,AS,T,O,A,E}
     buffer::MultiStepDynamicBuffer{O,A}
@@ -15,19 +15,19 @@ mutable struct ContinousFunctionalPG{OS,AS,T <: Real,O <: StaticArray{Tuple{OS},
     # rng::MersenneTwister
 end
 
-function ContinousFunctionalPG(::Type{O}, ::Type{A}, n, start_learning_rate, end_learning_rate, learning_rate_decay, epochs, discount, encoder::E, start_cov::Float64, end_cov::Float64, cov_decay::Int; seed::Union{Nothing,UInt}=nothing) where {OS,AS,T <: Real,O <: StaticArray{Tuple{OS},T,1},A <: StaticArray{Tuple{AS},T,1},E <: AbstractEncoder{T}}
+function ContinousFunctionalPG(::Type{O}, ::Type{A}, n, start_learning_rate, end_learning_rate, learning_rate_decay, epochs, discount, encoder::E, start_cov::Float64, end_cov::Float64, cov_decay::Int; seed::Union{Nothing,UInt} = nothing) where {OS,AS,T<:Real,O<:StaticArray{Tuple{OS},T,1},A<:StaticArray{Tuple{AS},T,1},E<:AbstractEncoder{T}}
     # !isnothing(seed) && seed < 0 && throw(DomainError(seed, "Seed must be non-negative"))
     # rng = isnothing(seed) ? MersenneTwister() : MersenneTwister(seed)
 
     n < 1 && throw(DomainError(n, "Tuple size must be at least 1"))
 
-    policy = ContinousActionPolicy(O, A, n, encoder;start_cov, end_cov, cov_decay, start_learning_rate, end_learning_rate, learning_rate_decay, epochs, partitioner=:uniform_random, seed)
+    policy = ContinousActionPolicy(O, A, n, encoder; start_cov, end_cov, cov_decay, start_learning_rate, end_learning_rate, learning_rate_decay, epochs, partitioner = :uniform_random, seed)
     buffer = MultiStepDynamicBuffer{O,A}()
 
     ContinousFunctionalPG{OS,AS,T,O,A,E}(discount, policy, buffer, nothing, false, nothing, nothing, nothing)
 end
 
-function ContinousFunctionalPG(env, encoder::E; tuple_size, start_learning_rate, end_learning_rate, learning_rate_decay, epochs, discount, start_cov, end_cov, cov_decay, seed=nothing) where {T <: Real,E <: AbstractEncoder{T},C <: AbstractMatrix{T}}
+function ContinousFunctionalPG(env, encoder::E; tuple_size, start_learning_rate, end_learning_rate, learning_rate_decay, epochs, discount, start_cov, end_cov, cov_decay, seed = nothing) where {T<:Real,E<:AbstractEncoder{T},C<:AbstractMatrix{T}}
     # ContinousFunctionalPG{action_length(env),T,observation_type(env),action_type(env),E,C}(n, observation_length(env), η, γ, encoder, cov; seed)
     ContinousFunctionalPG(observation_type(env), action_type(env), tuple_size, start_learning_rate, end_learning_rate, learning_rate_decay, epochs, discount, encoder, start_cov, end_cov, cov_decay; seed)
 end
@@ -61,15 +61,19 @@ function observe!(agent::ContinousFunctionalPG{OS,AS,T,O,A,E}, action::A, reward
     agent.observation = observation
     agent.done = done
 
-  # Finding the edges of the domain
+    # Finding the edges of the domain
     agent.min_obs = min.(agent.min_obs)
     agent.max_obs = max.(agent.max_obs)
 
     nothing
 end
 
+function select_action(agent::ContinousFunctionalPG{OS,AS,T,O,A,E}, observation::O; deterministic::Bool = false) where {OS,AS,T,O,A,E}
+    return select_action(agent.policy, observation; deterministic)
+end
+
 function select_action!(agent::ContinousFunctionalPG{OS,AS,T,O,A,E}, observation::O) where {OS,AS,T,O,A,E}
-    agent.action = select_action(agent.policy, observation)
+    agent.action = select_action(agent, observation)
 
     return agent.action
 end
@@ -89,7 +93,7 @@ function update!(agent::ContinousFunctionalPG)
     nothing
 end
 
-function Agents.reset!(agent::ContinousFunctionalPG; seed::Union{Nothing,UInt}=nothing)
+function Agents.reset!(agent::ContinousFunctionalPG; seed::Union{Nothing,UInt} = nothing)
     reset!(agent.policy; seed)
     reset!(agent.buffer)
 
--- src/Approximators/PolicyApproximators/ContinousActionPolicy.jl
+++ src/Approximators/PolicyApproximators/ContinousActionPolicy.jl
@@ -2,9 +2,9 @@ import ..update!, ..select_action, ..reset!
 
 using StaticArrays
 using Random
-using LinearAlgebra:Diagonal
+using LinearAlgebra: Diagonal
 
-using Ramnet.Models.AltDiscriminators:FunctionalDiscriminator
+using Ramnet.Models.AltDiscriminators: FunctionalDiscriminator
 using Ramnet.Optimizers
 import Ramnet.Loss
 
@@ -20,13 +20,13 @@ function ContinousActionPolicyLoss(start_cov::Float64, end_cov::Float64, cov_dec
 end
 
 # Policy approximator with only two admissible actions: {-1, 1}.
-struct ContinousActionPolicy{OS,AS,T <: Real,O <: StaticArray{Tuple{OS},T,1},A <: StaticArray{Tuple{AS},T,1},E <: AbstractEncoder{T}}
+struct ContinousActionPolicy{OS,AS,T<:Real,O<:StaticArray{Tuple{OS},T,1},A<:StaticArray{Tuple{AS},T,1},E<:AbstractEncoder{T}}
     opt::FunctionalOptimizer{ContinousActionPolicyLoss}
     f::FunctionalDiscriminator{AS,T,E}
     rng::MersenneTwister
 end
 
-function ContinousActionPolicy(::Type{O}, ::Type{A}, n::Int, encoder::E; start_cov::Float64, end_cov::Float64, cov_decay::Int, start_learning_rate::Float64=0.1, end_learning_rate::Float64=1e5, learning_rate_decay::Int=1000, epochs::Int=1, partitioner::Symbol=:uniform_random, seed::Union{Nothing,UInt}=nothing) where {AS,OS,T <: Real,O <: StaticArray{Tuple{OS},T,1},A <: StaticArray{Tuple{AS},T,1},E <: AbstractEncoder{T}}
+function ContinousActionPolicy(::Type{O}, ::Type{A}, n::Int, encoder::E; start_cov::Float64, end_cov::Float64, cov_decay::Int, start_learning_rate::Float64 = 0.1, end_learning_rate::Float64 = 1e5, learning_rate_decay::Int = 1000, epochs::Int = 1, partitioner::Symbol = :uniform_random, seed::Union{Nothing,UInt} = nothing) where {AS,OS,T<:Real,O<:StaticArray{Tuple{OS},T,1},A<:StaticArray{Tuple{AS},T,1},E<:AbstractEncoder{T}}
     ContinousActionPolicy{OS,AS,T,O,A,E}(
         FunctionalOptimizer(ContinousActionPolicyLoss(start_cov, end_cov, cov_decay); start_learning_rate, end_learning_rate, learning_rate_decay, epochs),
         FunctionalDiscriminator{AS}(OS, n, encoder, partitioner; seed),
@@ -43,23 +43,32 @@ function Loss.grad(l::ContinousActionPolicyLoss, action, pred_action)
     inv(cov(l)) * (pred_action - action)
 end
 
+function mode(p::ContinousActionPolicy{OS,AS,T,O,A,E}, observation::O) where {OS,AS,T,O,A,E}
+    # println(typeof(predict(p.f, observation)))
+    return SVector{AS,T}(predict(p.f, observation))
+end
+
 function update!(p::ContinousActionPolicy{OS,AS,T,O,A,E}, observation::O, action::A, G) where {OS,AS,T,O,A,E}
     # gradient = Optimizers.grad(p.opt, p, observation, action, G)
 
     gradient = Optimizers.grad(p.opt, action, predict(p.f, observation))
     train!(p.f, observation, learning_rate(p.opt) * gradient * G)
-    
+
     p.opt.loss.update_count += 1
-    
+
     return nothing
 end
 
 # TODO: The clamping of the action is specific to the hopper agent!
-function select_action(p::ContinousActionPolicy{OS,AS,T,O,A,E}, observation::O) where {OS,AS,T,O,A,E}
+function select_action(p::ContinousActionPolicy{OS,AS,T,O,A,E}, observation::O; deterministic::Bool = False) where {OS,AS,T,O,A,E}
+    if deterministic
+        return mode(p, observation)
+    end
+
     return A(cov(p.opt.loss) * randn(p.rng, AS) + predict(p.f, observation))
 end
 
-function reset!(p::ContinousActionPolicy; seed::Union{Nothing,UInt}=nothing)
+function reset!(p::ContinousActionPolicy; seed::Union{Nothing,UInt} = nothing)
     Ramnet.reset!(p.opt)
     p.opt.loss.update_count = 0
     Ramnet.reset!(p.f; seed)
